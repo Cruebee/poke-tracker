ad618af3b020faf01bc645e6ee54731091489225
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-02-11 16:35:57 -0700

    add evolution levels and final evolutions to each object within repository.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 // JavaScript sheet
 var repository = [
-  {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
-  {name: 'Ivysaur', height: 1, type: ['grass', 'poison']},
-  {name: 'Venusaur', height: 2, type: ['grass', 'poison']},
-  {name: 'Charmander', height: 0.6, type: ['fire']},
-  {name: 'Charmeleon', height: 1.1, type: ['fire']},
-  {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
-  {name: 'Squirtle', height: 0.5, type: ['water']},
-  {name: 'Wartortle', height: 1, type: ['water']},
-  {name: 'Blastoise', height: 1.6, type: ['water']}
+  {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'], evolution: 'at level' + 16},
+  {name: 'Ivysaur', height: 1, type: ['grass', 'poison'], evolution: 'at level' + 32},
+  {name: 'Venusaur', height: 2, type: ['grass', 'poison'], evolution: 'final evolution'},
+  {name: 'Charmander', height: 0.6, type: ['fire'], evolution: 'at level' + 16},
+  {name: 'Charmeleon', height: 1.1, type: ['fire'], evolution: 'at level' + 36},
+  {name: 'Charizard', height: 1.7, type: ['fire', 'flying'], evolution: 'final evolution'},
+  {name: 'Squirtle', height: 0.5, type: ['water'], evolution: 'at level' + 16},
+  {name: 'Wartortle', height: 1, type: ['water'], evolution: 'at level' + 36},
+  {name: 'Blastoise', height: 1.6, type: ['water'], evolution: 'final evolution'}
 ];
