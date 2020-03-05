2f70b7cb18a70a1d7c2a07828fef902d68d8d8a9
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-03-05 13:49:59 -0700

    Edit notes, and remove unused code

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,9 @@ function addListItem(pokemon) {
       console.log(item);
     });
   }
-
+/* The loadList method will fetch data from the API, then add it to repository with the add function implemented earlier.
+you want each item to have a name and a detailsUrl property. Use detailsUrl property to load detailed data for a given pokemon.
+To do that add in the loadDetails() function (which has been added below loadList function) */
 // This function loads the list of pokemon from the API:
   function loadList() {
     return fetch(apiUrl).then(function (response) {
@@ -91,9 +93,7 @@ function addListItem(pokemon) {
 })();
 // END IIFE POKEDEX REPOSITORY
 
-/* The loadList method will fetch data from the API, then add it to repository with the add function implemented earlier.
-you want each item to have a name and a detailsUrl property. Use detailsUrl property to load detailed data for a given pokemon.
-To do that add in the loadDetails() function */
+
 pokemonRepository.loadList().then(function() {
   // Now the data is loaded!
   pokemonRepository.getAll().forEach(function(pokemon){
@@ -105,91 +105,3 @@ pokemonRepository.loadList().then(function() {
   console.log(pokemonRepository.search('ivy').map(function(pokemon){
     return pokemon.name
   }))
-
-
-// Old forEach to iterate over repository array.
-/*
-pokemonRepository.getAll().forEach(function(currentItem){ // updates the forEach loop to use the .getAll function to print array.
-  if (currentItem.height > 0.7 + 'm'){
-  document.write('<p>' + currentItem.name + ' (height: ' + currentItem.height + ')' + ' -WOW! That\'s a bigg\'n.</p>');
-  }else{
-    document.write('<p>' + currentItem.name + ' (height: ' + currentItem.height + ')' );
-  }
-});
-*/
-
-// Create a for loop that iterates over each item in repository and use document.write() inside the loop's code to write the pokemon name on website.
-/* Here is the longer (standard (for) loop method.) above is the shorter cleaner version
-for (var i = 0; i < repository.length; i++){
-  if (repository[i].height > 0.7){ // If height is greater than 0.7...
-    document.write(repository[i].name + ' (height: ' + repository[i].height + ')' + ' -Wow!! That\'s a bigg\'n' + '<br>') // Write name and height with message -Wow!! That's a bigg'n.
-  }else{ // If height is not greater than 0.7
-    document.write(repository[i].name + ' (height: ' + repository[i].height + ')<br>') // Only write the name and height.
-  }
-}
-
-Here is the Repository Created for building the inital app:
-
-{name: 'Bulbasaur',
-height: 0.7 + 'm',
-type: ['grass', 'poison'],
-evolution: 'at level' + 16},
-
-{name: 'Ivysaur',
-height: 1 + 'm',
-type: ['grass', 'poison'],
-evolution: 'at level' + 32},
-
-{name: 'Venusaur',
-height: 2 + 'm',
-type: ['grass', 'poison'],
-evolution: 'final evolution'},
-
-{name: 'Charmander',
-height: 0.6 + 'm',
-type: ['fire'],
-evolution: 'at level' + 16},
-
-{name: 'Charmeleon',
-height: 1.1 + 'm',
-type: ['fire'],
-evolution: 'at level' + 36},
-
-{name: 'Charizard',
-height: 1.7 + 'm',
-type: ['fire', 'flying'],
-evolution: 'final evolution'},
-
-{name: 'Squirtle',
-height: 0.5 + 'm',
-type: ['water'],
-evolution: 'at level' + 16},
-
-{name: 'Wartortle',
-height: 1 + 'm',
-type: ['water'],
-evolution: 'at level' + 36}, // Note the difference between this object's readablitity and the one below it.
-// The more properties an object has the harder it will be to read if you keep them all on one line as seen below:
-{name: 'Blastoise', height: 1.6 + 'm', type: ['water'], evolution: 'final evolution'}
-];
-
-// This function will add a pokemon to the pokemonRepository if the pokemon to be added fits the correct format set by the if else statements.
-  function add(pokemon) {
-    if (typeof pokemon === 'object' && Object.keys(pokemon).every(p => ['name', 'height', 'type', 'evolution'].includes(p))) { // The p inside the () on .every and .includes is a placeholder rep for "pokemon".
-    repository.push(pokemon);
-    }else{
-        console.log('Unable to add Pokemon. Use format: [\'name\', \'height\', \'type\', \'evolution\'] .')
-    }
-  }
-
-// This calls the add function to act on the pokemonRepository telling it to add a pokemon which fits the correct format.
-// Note must be added before the forEach loop that iterates over all of the pokemon objects in the pokemonRepository array.
-pokemonRepository.add({name: 'Pikachu', height: 0.4 + 'm', type: ['electric'], evolution: 'evolves with stone'});
-
-var $pokemonList = document.querySelector('.pokemon-list');
-
-// More concise and clean forEach method.
-pokemonRepository.getAll().forEach(function(addListItem){
-  pokemonRepository.addListItem(addListItem.name);
-});
-*/
